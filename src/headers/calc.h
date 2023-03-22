8a7e4d7cd8261bdbffa64cf7ae83ab95e0d2b0c1
Victoria Brunner <astrobrunner@gmail.com>
2023-03-21 21:00:06 -0400

    write calculation function header files

---
+++ src/headers/calc.h
@@ -0,0 +1,12 @@
+#ifndef CALC_H
+#define CALC_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+double add(double a, double b);
+double sub(double a, double b);
+double mult(double a, double b);
+double div (double a, double b);
+
+#endif
