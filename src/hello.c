a441644e1bef306c6733f9a5d5fe8d601646379e
Victoria Brunner <astrobrunner@gmail.com>
2023-03-21 22:05:03 -0400

    test calculation functions with defined numbers

--- src/hello.c
+++ src/hello.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
+#include "calc.h"
 
 int main(){
-	printf("Hello World!");
-	printf("This is Astro");
+	double x = 2.00;
+	double y = 2.00;
+
+	printf("Hello World!\n");
+	printf("This is Astro\n\n");
+
+	printf("%.2f + %.2f is %.2f\n", x, y, add(x, y));
+	printf("%.2f - %.2f is %.2f\n", x, y, sub(x, y));
+	printf("%.2f * %.2f is %.2f\n", x, y, mult(x, y));
+	printf("%.2f / %.2f is %.2f\n", x, y, div(x, y));
+	printf("\n");
+
+	return 0;
 }
